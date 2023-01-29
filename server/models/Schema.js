99ec75a26287b5a6b23cff2e80a1cf2d26e1f2e0
hasanmd91 <hasanmd1991@hotmail.com>
2023-01-29 13:41:50 +0200

    fix schema added trim:true

--- server/models/Schema.js
+++ server/models/Schema.js
@@ -3,14 +3,14 @@ import mongoose from "mongoose";
 // journey details schema
 
 const journeySchema = mongoose.Schema({
-  Departure_time: String,
-  Return_time: String,
-  Departure_Station_Id: Number,
-  Departure_Station_Name: String,
-  Return_Station_Id: Number,
-  Return_Station_Name: String,
-  Covered_Distance: Number,
-  Duration_of_journey: Number,
+  Departure_time: { type: String, trim: true },
+  Return_time: { type: String, trim: true },
+  Departure_Station_Id: { type: Number, trim: true },
+  Departure_Station_Name: { type: String, trim: true },
+  Return_Station_Id: { type: Number, trim: true },
+  Return_Station_Name: { type: String, trim: true },
+  Covered_Distance: { type: Number, trim: true },
+  Duration_of_journey: { type: Number, trim: true },
 });
 
 export const journey_details = mongoose.model("journey_details", journeySchema);
@@ -18,15 +18,15 @@ export const journey_details = mongoose.model("journey_details", journeySchema);
 // station schema
 
 const stationListSchema = mongoose.Schema({
-  Fid: Number,
-  Id: Number,
-  Nimi: String,
-  Osoite: String,
-  Adress: String,
-  kaupunki: String,
-  Kapasiteet: Number,
-  x: Number,
-  y: Number,
+  Fid: { type: Number, trim: true },
+  Id: { type: Number, trim: true },
+  Nimi: { type: String, trim: true },
+  Osoite: { type: String, trim: true },
+  Adress: { type: String, trim: true },
+  kaupunki: { type: String, trim: true },
+  Kapasiteet: { type: Number, trim: true },
+  x: { type: Number, trim: true },
+  y: { type: Number, trim: true },
 });
 
 export const station_details = mongoose.model(
