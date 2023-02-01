5097e95acb3b9f7c084f75a169abbac242abffa0
hasanmd91 <hasanmd1991@hotmail.com>
2023-02-01 23:21:46 +0200

    build readme file

--- client/src/Components/Singlestation/Map.tsx
+++ client/src/Components/Singlestation/Map.tsx
@@ -8,7 +8,10 @@ interface props {
   title: string;
 }
 
+// google map component
+
 const Map: React.FC<props> = ({ x, y, title }) => {
+  // useLoadScript is a hook to load the Google Maps JavaScript API
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyBcoxSMxAWa8QurUTzORx-9GQLqr84wqbg",
   });
@@ -18,7 +21,11 @@ const Map: React.FC<props> = ({ x, y, title }) => {
 };
 
 const LocationMap: React.FC<props> = ({ x, y, title }) => {
+  // useMemo is a hook that memoizes values, so they are not recreated if not needed
+  // In this case, we are only creating the center object once, with the given x and y values
+
   const center = useMemo(() => ({ lng: x, lat: y }), [x, y]);
+  // Render a GoogleMap with a Marker in the given center position and title
 
   return (
     <GoogleMap
